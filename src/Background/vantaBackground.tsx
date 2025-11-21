d49b44c7aa42ee2d022f8087470b38e7c8e96e79
med1seghir5 <mohamed.abdelmalek@etu.univ-batna2.dz>
2025-11-21 01:05:56 +0100

    another commi

--- src/Background/vantaBackground.tsx
+++ src/Background/vantaBackground.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Canvas, } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
 
@@ -20,26 +19,3 @@ const VantaBackground = () => {
 };
 
 export default VantaBackground;
-=======
-import { Canvas, } from "@react-three/fiber";
-import { Stars } from "@react-three/drei";
-
-const VantaBackground = () => {
-  return (
-    <div
-      className="fixed inset-0 -z-50"
-      style={{
-        background: "linear-gradient(62deg, #100425 0%, #100425 100%)",
-      }}
-    >
-      <Canvas camera={{ position: [0, 0, 10] }}>
-        <ambientLight intensity={5.5} />
-        <pointLight position={[10, 10, 10]} />
-        <Stars radius={100} depth={50} count={5000} factor={4} fade />
-      </Canvas>
-    </div>
-  );
-};
-
-export default VantaBackground;
->>>>>>> 476f160ad299c7d325babf7aed63cb2af376122b
--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import Tilty from "react-tilty";
 
 const logos = [
@@ -43,49 +41,3 @@ const Skills: React.FC = () => {
 };
 
 export default Skills;
-=======
-
-import Tilty from "react-tilty";
-
-const logos = [
-  { name: "HTML5", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
-  { name: "CSS3", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
-  { name: "JavaScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-  { name: "TypeScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"  },
-  { name: "Node JS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
-  { name: "Express JS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" },
-  { name: "MongoDB", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" },
-  { name: "React JS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-  { name: "Next JS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
-  { name: "Tailwind", src: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg" },
-  { name: "MySQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" },
-  { name: "PostgreSQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" },
-  { name: "C", src: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/926px-C_Programming_Language.svg.png" },
-  { name: "Python", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
-  { name: "Git", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" },
-  { name: "GitHub", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" },
-];
-
-const Skills: React.FC = () => {
-  return (
-    <section className="w-full py-36 flex flex-col items-center gap-10 mt-48">
-      <h2 className="title text-4xl font-bold bg-linear-to-r from-[#5DFFFF] via-[#8A7FF0] to-[#AE0CA7] bg-clip-text text-transparent">Skills</h2>
-      <div className="skills-text grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-16">
-        {logos.map((logo, index) => (
-          <Tilty
-            key={index}
-            max={25}
-            scale={1.6}
-            className="bg-white/10 hover:bg-white/30 transition p-5 rounded-2xl cursor-pointer shadow-lg backdrop-blur-md flex flex-col items-center justify-center"
-          >
-            <img src={logo.src} alt={logo.name} className="w-16 h-16 object-contain" />
-            <p className="text-white mt-2 font-semibold text-sm">{logo.name}</p>
-          </Tilty>
-        ))}
-      </div>
-    </section>
-  );
-};
-
-export default Skills;
->>>>>>> 476f160ad299c7d325babf7aed63cb2af376122b
